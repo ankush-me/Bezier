e6ce5a1b43bec1618992064bb1ace8dc598ef51e
Ankush Gupta <ankush.me@hotmail.com>
2013-04-05 00:42:36 -0700

    added rotation/ translation/ zoom/ wireframe/ shading keys

--- src/tests/dispApp.cpp
+++ src/tests/dispApp.cpp
@@ -19,8 +19,9 @@
 
 using namespace std;
 
-#define VIEWING_DISTANCE_MIN  8.9
+#define VIEWING_DISTANCE_MIN  2.0
 #define TEXTURE_ID_CUBE 1
+
 enum {
 	MENU_LIGHTING = 1,
 	MENU_POLYMODE,
@@ -37,11 +38,18 @@ static BOOL g_bFillPolygons    = TRUE;
 static BOOL g_bSmoothShading   = TRUE;
 static BOOL g_bButton1Down     = FALSE;
 static GLfloat g_fTeapotAngle  = 0.0;
-static GLfloat g_fTeapotAngle2 = 0.0;
 static GLfloat g_fInitViewDistance = VIEWING_DISTANCE_MIN;
 static GLfloat g_fViewDistance = 1.2*VIEWING_DISTANCE_MIN;
 static GLfloat g_nearPlane     = 1;
 static GLfloat g_farPlane      = 1000;
+
+// variables to rotate/ translate the object.
+static GLfloat g_angleX        = 0;
+static GLfloat g_angleY        = 0;
+static GLfloat g_angleZ        = 0;
+static GLfloat g_transX        = 0;
+static GLfloat g_transY        = 0;
+
 static int g_Width            = 600;                          // Initial window width
 static int g_Height           = 600;                         // Initial window height
 static int g_yClick            = 0;
@@ -53,19 +61,20 @@ const float colorBlue[4]       = { 0.0, 0.2, 1.0, 1.0 };
 const float colorNone[4]       = { 0.0, 0.0, 0.0, 0.0 };
 
 void RenderObjects(void) {
-	glMatrixMode(GL_MODELVIEW);  // edit the model view matrix
+	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 
 	// Child object (teapot) ... relative transform, and render
-	glTranslatef(0, 0, -7.5);
-	glRotatef(g_fTeapotAngle2, 1, 1, 0);
+	glTranslatef(-g_transX, g_transY, 0.0);
+	glRotatef(g_angleX, 1, 0, 0);
+	glRotatef(g_angleY, 0, 1, 0);
+
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, colorBronzeDiff);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, colorBronzeSpec);
 	glMaterialf(GL_FRONT, GL_SHININESS, 50.0);
 	glColor4fv(colorBronzeDiff);
 
 	glutSolidTeapot(0.3);
-
 	glPopMatrix();
 }
 
@@ -106,12 +115,10 @@ void InitGraphics(void) {
 	glEnable(GL_LIGHT0);
 }
 
-void MouseButton(int button, int state, int x, int y)
-{
+void MouseButton(int button, int state, int x, int y) {
 	// Respond to mouse button presses.
 	// If button1 pressed, mark this state so we know in motion function.
-	if (button == GLUT_LEFT_BUTTON)
-	{
+	if (button == GLUT_LEFT_BUTTON) {
 		g_bButton1Down = (state == GLUT_DOWN) ? TRUE : FALSE;
 		g_fInitViewDistance = g_fViewDistance;
 		g_yClick = y;
@@ -120,10 +127,8 @@ void MouseButton(int button, int state, int x, int y)
 
 void MouseMotion(int x, int y) {
 	// If button1 pressed, zoom in/out if mouse is moved up/down.
-	if (g_bButton1Down)
-	{
+	if (g_bButton1Down) {
 		g_fViewDistance = g_fInitViewDistance + (y - g_yClick) / 20.0;
-		cout<< g_fViewDistance << endl;
 		if (g_fViewDistance < VIEWING_DISTANCE_MIN)
 			g_fViewDistance = VIEWING_DISTANCE_MIN;
 		glutPostRedisplay();
@@ -148,33 +153,81 @@ void SelectFromMenu(int idCommand) {
 		glPolygonMode (GL_FRONT_AND_BACK, g_bFillPolygons ? GL_FILL : GL_LINE);
 		break;
 	case MENU_SHADING:
-			g_bSmoothShading = !g_bSmoothShading;
-			glShadeModel(g_bSmoothShading? GL_SMOOTH : GL_FLAT);
-			break;
+		// OpenGL does not have Phong shading.
+		// So smooth shading means Gouraud shading which means the
+		// colors are interpolated from the vertices.
+		g_bSmoothShading = !g_bSmoothShading;
+		glShadeModel(g_bSmoothShading? GL_SMOOTH : GL_FLAT);
+		break;
 	case MENU_EXIT:
 		exit (0);
 		break;
 	}
-	// Almost any menu selection requires a redraw
+	// redraw
 	glutPostRedisplay();
 }
+
+void KeyBoardArrows (int key, int x, int y) {
+	switch(key) {
+	case GLUT_KEY_LEFT:
+		if (glutGetModifiers() == GLUT_ACTIVE_SHIFT) { // translate
+			g_transX -= 0.1;
+		} else { // rotate
+			g_angleY-= 5;
+		}
+		break;
+	case GLUT_KEY_RIGHT:
+		if (glutGetModifiers() == GLUT_ACTIVE_SHIFT) { // translate
+			g_transX += 0.1;
+		} else { // rotate
+			g_angleY += 5;
+		}
+		break;
+	case GLUT_KEY_UP:
+		if (glutGetModifiers() == GLUT_ACTIVE_SHIFT) { // translate
+			g_transY += 0.1;
+		} else { // rotate
+			g_angleX += 5;
+		}
+		break;
+	case GLUT_KEY_DOWN:
+		if (glutGetModifiers() == GLUT_ACTIVE_SHIFT) { // translate
+			g_transY -= 0.1;
+		} else { // rotate
+			g_angleX -= 5;
+		}
+		break;
+	}
+}
+
 
 void Keyboard(unsigned char key, int x, int y) {
 	switch (key) {
 	case 27:  // ESCAPE
 		exit (0);
 		break;
-	case 'l':
+	case 'l': // light/ no light
 		SelectFromMenu(MENU_LIGHTING);
 		break;
-	case 'w':
+	case 'w':  // filled/ wireframe
 		SelectFromMenu(MENU_POLYMODE);
 		break;
-	case 's':
+	case 's':  // smooth/ flat shading
 		SelectFromMenu(MENU_SHADING);
+		break;
+	case '+': // zoom in
+		g_fViewDistance = g_fViewDistance - 0.05;
+		if (g_fViewDistance < VIEWING_DISTANCE_MIN)
+			g_fViewDistance = VIEWING_DISTANCE_MIN;
+		glutPostRedisplay();
+		break;
+	case '-': // zoom in
+		g_fViewDistance = g_fViewDistance + 0.05;
+		glutPostRedisplay();
 		break;
 	}
 }
+
 
 int BuildPopupMenu (void) {
 	int menu;
@@ -194,22 +247,23 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow ("CS184  - Bezier Subdivision");
 
-	// Initialize OpenGL graphics state
+	// initialize OpenGL graphics state
 	InitGraphics();
 
-	// Register callbacks:
-	glutDisplayFunc (display);
-	glutReshapeFunc (reshape);
-	glutKeyboardFunc (Keyboard);
-	glutMouseFunc (MouseButton);
-	glutMotionFunc (MouseMotion);
-	glutIdleFunc (AnimateScene);
+	// register callbacks:
+	glutDisplayFunc(display);
+	glutReshapeFunc  (reshape);
+	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(KeyBoardArrows);  // to handle arrow keys
+	glutMouseFunc(MouseButton);
+	glutMotionFunc(MouseMotion);
+	glutIdleFunc(AnimateScene);
 
-	// Create our popup menu
-	BuildPopupMenu ();
-	glutAttachMenu (GLUT_RIGHT_BUTTON);
+	// create popup menu
+	BuildPopupMenu();
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 	// Turn the flow of control over to GLUT
-	glutMainLoop ();
+	glutMainLoop();
 	return 0;
 }
